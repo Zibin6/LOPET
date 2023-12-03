b991d075088cafcbb72c8b68e26d244ae1f43c00
Zibin Liu <102710081+Zibin6@users.noreply.github.com>
2023-12-03 21:51:52 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,28 @@
+
+This repository contains the demo code of Our-MM method and dataset.
+
+Demo code has been validated on MATLAB 2019.
+
+Details of the method can be found in our paper:
+
+Line-based 6-DoF Object Pose Estimation and Tracking With an Event Camera
+
+Zibin Liu, Banglei Guan, Yang Shang, Qifeng Yu, and Laurent Kneip.
+
+The paper is currently under review and will be made public soon.
+
+2023/07/15
+
+
+## Dataset
+
+The event-based moving object dataset can be found at:
+ 
+https://pan.baidu.com/s/1wOTAbeAUkNjK4Gnj8ZazaA?pwd=ztwm 
+
+
+## Contact
+
+Please contact Banglei Guan <guanbanglei12@nudt.edu.cn> or any of the other authors for any inquiries or requests.
+
+
---
+++ main.m
@@ -0,0 +1,53 @@
+
+clc
+clear
+close all
+addpath("func\")
+load("demo.mat");
+
+
+%Camera intrinsic matrix
+K=[1654 0 651;0 1653 368;0 0 1];
+    
+%Initial pose estimation
+
+fprintf('Start initial pose estimation. Please wait.');
+
+pose_cur=initial_mypose();
+
+fprintf('\nThe initial pose estimation is completed, and the result is:\n');
+
+pose_cur
+
+fprintf('\nStart pose tracking');
+
+
+ for t=cube(11000,1):0.01:cube(end,1)
+
+     if t<(cube(end,1)-0.01)
+
+         % Event - line matching
+
+         [event_cluster event_cur]=elmatch(K,pose_cur,t,cube);
+ 
+         %Pose optimization
+
+         fprintf('\nThe optimized pose is:');    
+
+         [pose_cur] =pose_optim(event_cluster, pose_cur(1:3,1:3),pose_cur(1:3,4),K)
+
+         %Pose reprojection
+
+         [repro_Image]=reproj_show(K,pose_cur,event_cur);
+
+         imshow(repro_Image); 
+
+     else
+
+         fprintf('\nThe demo ends here.');
+
+         return;
+
+     end
+  
+end
